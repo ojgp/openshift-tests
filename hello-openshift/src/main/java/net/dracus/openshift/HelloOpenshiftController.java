7a641ffb5bf42798bf3d9c55199dd9d8cf1a1667
Gómez Plietz <ojgomez@minsait.com>
2022-05-26 08:00:49 +0200

    Adding Openshit test application

---
+++ hello-openshift/src/main/java/net/dracus/openshift/HelloOpenshiftController.java
@@ -0,0 +1,22 @@
+package net.dracus.openshift;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/hello", produces = "application/json")
+public class HelloOpenshiftController {
+
+	@GetMapping("")
+	public String sayHello() {
+		return "Openshift test application";
+	}
+
+	@GetMapping("{message}")
+	public String sayHello(@PathVariable String message) {
+		return "Hello " + message;
+	}
+
+}
