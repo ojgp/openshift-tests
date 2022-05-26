ac0e3a3ff45ee207be7695c5a66e5bb5702a72d4
Gómez Plietz <ojgomez@minsait.com>
2022-05-26 10:23:41 +0200

    Printing variables

--- hello-openshift/src/main/java/net/dracus/openshift/HelloOpenshiftController.java
+++ hello-openshift/src/main/java/net/dracus/openshift/HelloOpenshiftController.java
@@ -1,5 +1,6 @@
 package net.dracus.openshift;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/hello", produces = "application/json")
 public class HelloOpenshiftController {
+	
+	@Value("${dracus.msg1}")
+	private String msg1;
+	
+	@Value("${dracus.msg2}")
+	private String msg2;
 
 	@GetMapping("")
 	public String sayHello() {
-		return "Openshift test application\n";
+		var result = "Openshift test application\n";
+		result = result + "Message 1: " + msg1 + "\n";
+		result = result + "Message 2: " + msg2 + "\n";
+		return result;
 	}
 
 	@GetMapping("{message}")
